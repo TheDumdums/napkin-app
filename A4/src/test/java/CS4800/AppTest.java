dc29a99a9bdccdf0e29bc6ea1dd6f462670d7720
JubeMhae <jcucal@cpp.edu>
2023-02-23 15:07:29 -0800

    A4p1p2

---
+++ A4/src/test/java/CS4800/AppTest.java
@@ -0,0 +1,20 @@
+package CS4800;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
