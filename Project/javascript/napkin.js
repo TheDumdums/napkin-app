11e13a888766b91fc9fdce5bcea08e1c6caf7946
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2023-02-24 23:06:56 -0800

    Revamped css and added signature pad

--- Project/javascript/napkin.js
+++ Project/javascript/napkin.js
@@ -1,5 +1,29 @@
-const canvas = document.querySelector("napkin");
-const signaturePad = new SignaturePad(canvas);
-signaturePad.addEventListener("beginStroke", () => {
-  console.log("Signature started");
-}, { once: true });
+var napkinCanvas = document.getElementById('napkin')
+
+var signaturePad = new SignaturePad(napkinCanvas, {
+    minWidth: 3,
+    maxWidth: 8
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === 'z') {
+        console.log("Undo key");
+        var data = signaturePad.toData();
+        if (data) {
+            data.pop();
+        }
+        signaturePad.fromData(data);
+    }
+});
+
+function colorChangeBlack() {
+    signaturePad.penColor = 'rgb(0,0,0)';
+}
+
+function colorChangeRed() {
+    signaturePad.penColor = 'rgb(203,5,5)';
+}
+
+function colorChangeBlue() {
+    signaturePad.penColor = 'rgb(21,18,193)';
+}
