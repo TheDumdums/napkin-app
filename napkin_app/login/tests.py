99587d2a598377d0e8711496ed91ea42ce2499cb
JubeMhae <jcucal@cpp.edu>
2023-03-15 10:34:03 -0700

    Signup_and_login_Unit_test

--- napkin_app/login/tests.py
+++ napkin_app/login/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestCaseTesting(TestCase):
+    #Sign up and login test
+    def test_register_user(self):
+        #Sign up with new user
+        response = self.client.post('/signUp/', data={
+            'username': 'UnitTest',
+            'email': 'UnitTest@example.com',
+            'password': 'ShhhUnitTesting',
+            'password2': 'ShhhUnitTesting',
+        }, follow_redirects=True)
+        assert response.status_code == 200
+
+        # login with new user
+        response = self.client.post('/signIn/', data={
+            'email': 'UnitTest@example.com',
+            'password': 'ShhhUnitTesting',
+        }, follow_redirects=True)
+        assert response.status_code == 200
