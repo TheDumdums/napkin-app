955d30ea503627e440ff6a560470d3dd521a332f
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2023-03-17 21:47:04 -0700

    Added allowed host so EC2 can run it

--- napkin_app/napkin_app/settings.py
+++ napkin_app/napkin_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-51&xlp4)678y7dq6j6cn_9xx%)puz6ba=*ho+*^dfokh_jqym4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
